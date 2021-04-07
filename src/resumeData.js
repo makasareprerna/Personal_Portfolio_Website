bea380e6ab7c1e417e4eabaa71620233b8da22ef
Prerna Makasare <makasareprerna@gmail.com>
2021-04-07 18:02:37 -0500

    'test'

---
+++ src/resumeData.js
@@ -0,0 +1,124 @@
+let resumeData = {
+    //"imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Prerna Makasare",
+    "role": "Software Developer and Data Scientist",
+    "linkedinId":"https://www.linkedin.com/in/prernamakasare/",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/prernamakasare/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/makasareprerna",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"  I beleive that there is nothing to fear, because you cannot fail you could only learn, grow, and become better than you’ve ever been before. To be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"United States",
+    //"website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"University of Houston",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2021",
+        //"Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"MGM's J.N.E.C",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2018",
+        //"Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Computer Science Intern, Spool Performance, LLC",
+        //"specialization":"Some specialization",
+        "MonthOfLeaving":"Nov",
+        "YearOfLeaving":"2020",
+        "Achievements":
+        "Developed and handled iOS and android application which allows users to visualize parameters related to ethanol content in the car. Integrated Bluetooth low energy library which allows application to automatically connect devices to car and collect data. Gathered and implemented client requirements by constructing wireframes and mockups. Incorporated persistent data for specific user to achieve last visited state of the application. Technologies used: XCode, Swift, Android Studio, Android SDK, Java, XML, (Marvel, Sketch – Wireframes and mockups)."
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    //"skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5", level: 95
+      },
+      {
+        "skillname":"CSS", level: 80
+      },
+      {
+        "skillname":"Reactjs", level: 70
+      },
+      {
+        "skillname":"Javascript", level: 99
+      },
+      {
+        "skillname":"Nodejs", level: 95
+      },
+      {
+        "skillname":"Python", level: 95
+      },
+      {
+        "skillname":"Java", level: 95
+      },
+      {
+        "skillname":"csharp", level: 95
+      },
+
+      
+    ],
+    "portfolio":[
+      {
+        "name":"Digital Image Processing Transformation",
+        //"description":"mobileapp",
+        "imgurl":"images/portfolio/dip.png"
+      },
+      {
+        "name":"Grade Prediction Analysis",
+        //"description":"mobileapp",
+        "imgurl":"images/portfolio/Graph.jpg"
+      },
+      {
+        "name":"Plant Care Management Application",
+        //"description":"mobileapp",  
+        "imgurl":"images/portfolio/plant_care.jpg"
+      },
+      {
+        "name":"Algorithm Visualizer",
+        //"description":"mobileapp",
+        "imgurl":"images/portfolio/sort.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"If you don't like something, change it. If you can't change it, change your attitude.",
+        "name":"MAYA ANGELOU"
+      },
+      {
+        "description":"Life begins at the end of your comfort zone. ",
+        "name":"NEALE DONALD WALSH"
+      }
+    ]
+  }
+  
+  export default resumeData
